e1272bd5f5235900cfc8097c2e90dfdb772da9ac
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-03-30 16:31:55 +0700

    void display

--- quickShortt_115/quickShortt_115.cpp
+++ quickShortt_115/quickShortt_115.cpp
@@ -98,3 +98,14 @@ void q_short(int low, int high)
 
 }
 
+void display() {
+	cout << "\n---------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "---------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+
